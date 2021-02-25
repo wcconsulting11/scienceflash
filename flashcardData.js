bd91057ef34bfa82eb314b12065199944904bbf9
Wendy Cordero <wcconsulting11@gmail.com>
2021-02-25 14:40:47 +0000

    completed flashcard app

---
+++ flashcardData.js
@@ -0,0 +1,77 @@
+var importedFlashCardDataFile = [{
+    front: {
+      question: 'The sky is blue due to ________.',
+      hint: 'How sunlight passes through the earths atmosphere.'
+    },
+    back: {
+      image: './images/bluesky.JPG',
+      answer: 'scattering',
+      description: 'Blue light has a relatively short wavelength so it gets through the filter more easily than long wavelengths, resulting in a wider scattered of color.'
+    }
+  },
+  {
+    front: {
+      question: 'Why are bubbles round?',
+      hint: 'thin layers of liquid whose molecules stick together because they are attracted to one another, a phenomenon named.'
+    },
+    back: {
+      image: './images/bubbles.JPG',
+      answer: 'Cohesion',
+      description: 'surface tension, a barrier that resists objects trying to move through it. Inside the layer, air molecules that are trapped cannot get out, even though they are pushing against the water.'
+    }
+  },
+  {
+    front: {
+      question: 'The blueprints of a living organism is called?',
+      hint: 'I am often refered to as a 3 letter word.'
+    },
+    back: {
+      image: './images/DNA.JPG',
+      answer: 'DNA',
+      description: '(DNA)Deoxyribonucleic acid is a molecule composed of two polynucleotide chains that coil around each other to form a double helix.'
+    }
+  },
+  {
+    front: {
+      question: '_________ is the process where plants capture sunlight.',
+      hint: 'Plants take in carbon dioxide, water, and energy to make food for themselves.'
+    },
+    back: {
+      image: './images/process.JPG',
+      answer: 'Photosynthesis',
+      description: 'This is what it looks like: 6 CO2 + 6 H2O + light energy→ C6H12O6 + 6 O2.'
+    }
+  },
+  {
+    front: {
+      question: '_________ is the downward force that acts on objects.',
+      hint: 'Often shown in experiements using springs and pulleys.'
+    },
+    back: {
+      image: './images/gravity.jpg',
+      answer: 'Gravity',
+      description: 'Gravity or gravitational forces are forces of attraction.Every object in the universe that has mass exerts a gravitational pull, or force, on every other mass.'
+    }
+  },
+  {
+    front: {
+      question: '_________ energy is energy that results from an objects motion.',
+      hint: 'examples: airplanes flying, skiers going downhill, a car driving.'
+    },
+    back: {
+      image: './images/kinetic.jpg',
+      answer: 'Kinetic',
+      description: 'If an object is not moving, it has no kinetic energy.'
+    }
+  },
+  {
+    front: {
+      question: 'The ____ ___ is part of the Polar Circle.',
+      hint: 'Kids think santa and elves live there.'
+    },
+    back: {
+      image: './images/northpole.jpg',
+      answer: 'North Pole',
+      description: 'The North Pole is not true North and also has no land, just floating Ice.'
+    }
+  }];
